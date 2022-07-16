08599709d82f5cf81788b985a6d251a07495c404
Alextxnk <alexsolov766@gmail.com>
2022-07-16 22:19:49 +0300

    Lesson 03

--- lessons/lesson03conditions/homework03.js
+++ lessons/lesson03conditions/homework03.js
@@ -60,14 +60,82 @@ console.log('У вас средний уровень дохода');
    console.log('Что-то пошло не так');
 }
 
+
 // доп задание  
-/* 
-1. Переменная lang может принимать 2 значения: 'ru' 'en'.
-Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке. Решите задачу
-   a. через if, 
-   b. через switch-case 
-   c. через многомерный массив без ифов и switch.
+/* 1. Переменная lang может принимать 2 значения: 'ru' 'en'.
+Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке. 
+Решите задачу:
+   через if, 
+   через switch-case  */ 
 
-2. У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Максим” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
+let myDate = new Date(); // текущая дата 
+// console.log(myDate);
 
-Решить задачу с помощью нескольких тернарных операторов, без использования if или switch */
+let dayOfTheWeek = myDate.getDay(); // день недели текущей даты 
+let dayOfTheMonth = myDate.getDate(); // число месяца текущей даты 
+let numberOfMonth = myDate.getMonth(); // месяц текущей даты 
+let year = myDate.getFullYear(); // текущий год
+
+// данные для русскоязычного вывода в консоль 
+// массив с днями недели
+let ruDayOfTheWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let nameOfRuDayOfTheWeek = ruDayOfTheWeek[dayOfTheWeek]; // Название дня недели на русском языке
+
+// массив с месяцами
+let ruMonth = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня' , 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября' , 'Декабря'];
+let nameOfRuMonth = ruMonth[numberOfMonth]; // название месяца на русском языке из массива ruMonth
+
+// переменная для вывода в консоль 
+let ruLogVariable = 'Сегодня день недели: ' + nameOfRuDayOfTheWeek + '. ' + dayOfTheMonth + ' ' + nameOfRuMonth + ' ' + year + ' год'; 
+
+
+// данные для англоязычного вывода в консоль 
+// массив с днями недели 
+let enDayOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let nameOfEnDayOfTheWeek = enDayOfTheWeek[dayOfTheWeek]; // Название дня недели на английском языке
+
+// массив с месяцами
+let enMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let nameOfEnMonth = enMonth[numberOfMonth]; // название месяца на английском языке из массива enMonth
+
+// переменная для вывода в консоль
+let enLogVariable = 'Today is the day of the week: ' + nameOfEnDayOfTheWeek + '. ' + dayOfTheMonth + ' ' + nameOfEnMonth + ' ' + year + ' year';
+
+let lang = 'ru';
+// let lang = 'en';
+
+if (lang === 'ru') {
+   switch(dayOfTheWeek) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+      case 4: 
+      case 5:
+      case 6:
+         console.log(ruLogVariable);
+         break;
+      default:
+         console.log('Неправильно введена дата!');
+   }
+} else if (lang === 'en') {
+   if (dayOfTheWeek >= 0  && dayOfTheWeek <= 6) {
+      console.log(enLogVariable);
+   } else {
+      console.log('Date entered incorrectly!');
+   }
+} else {
+   console.log('Неправильно выбран язык! Выберите ru или en');
+}
+
+/* 2. У нас есть переменная namePerson. Если значение этой переменной 'Артем' то вывести в консоль 'директор', 
+если значение 'Максим' то вывести в консоль 'преподаватель', с любым другим значением вывести в консоль 'студент'
+   Решить задачу с помощью нескольких тернарных операторов, без использования if или switch */
+
+let namePerson = 'Артем'; 
+// let namePerson = 'Максим'; 
+
+let post1 = (namePerson === 'Артем') ? 'директор' : 'студент';
+let post2 = (namePerson === 'Максим') ? 'преподаватель' : 'студент';
+console.log(post1);
+console.log(post2);
