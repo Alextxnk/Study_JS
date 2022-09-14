3ee08e9eead7c9e189e9bf60b80cfe8af7c2e9d3
Alextxnk <alexsolov766@gmail.com>
2022-09-14 18:46:38 +0300

    homework10

--- lessons/homework10/index.js
+++ lessons/homework10/index.js
@@ -1,19 +1,10 @@
 'use strict';
 
 
-/* Используя только файл скрипта (html руками не трогать) выполнить такие действия:
-   + 1). Восстановить порядок книг.
-   + 2). Заменить картинку заднего фона на другую из папки image
-   3). Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
-   + 4). Удалить рекламу со страницы 
-   5). Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, 
-   поможет dev tools)
-   6). В шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место */
-
 const booksAside = document.querySelectorAll('.books');
-console.log(booksAside);
+// console.log(booksAside);
 const books = document.querySelectorAll('.book');
-console.log(books);
+// console.log(books);
 
 // 1). Восстановил порядок книг
 booksAside[0].prepend(books[1]); // ставим 1 элемент в начало
@@ -25,8 +16,53 @@ books[5].after(books[2]); // ставим 6 элемент на место
 document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 
 // 3). Исправил заголовок в книге 3
-
+const secondHead = document.querySelectorAll('h2'); // получилии тег h2
+// console.log(secondHead);
+// console.log(secondHead[2]);
+secondHead[2].innerHTML = '<a href="https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes" target="_blank">Книга 3. this и Прототипы Объектов</a>';
 
 // 4). убрал рекламу
 const advertisement = document.querySelector('.adv');
 advertisement.style = 'display: none';
+
+// 5). Восстановил порядок глав
+// Восстановить порядок глав во второй и пятой книге
+
+const collections = document.querySelectorAll('.collection');
+// console.log(collections); // 2 and 5 книги
+const elems = document.querySelectorAll('.elem');
+// console.log(elems); // li
+
+
+// console.log(collections[0]); // 2 книга
+// console.log(collections[1]); // 5 книга 
+
+// 2 книга
+elems[0].after(elems[1]);
+elems[1].after(elems[3]);
+elems[3].after(elems[6]);
+elems[6].after(elems[8]);
+elems[8].after(elems[4]);
+elems[4].after(elems[5]);
+elems[5].after(elems[7]);
+elems[9].after(elems[2]);
+
+// 5 книга
+elems[12].after(elems[20]);
+elems[20].after(elems[14]);
+elems[14].after(elems[15]);
+elems[18].after(elems[16]);
+
+
+// 6). В шестой книге добавить главу
+const bookNumSix = document.querySelector('.book-6'); // ul
+// console.log(bookNumSix);
+
+const newElem = document.createElement('li'); // создали новый li
+bookNumSix.append(newElem); // добавили в ul
+newElem.textContent = 'Глава 8: За пределами ES6'; 
+newElem.classList.add('book-6-li');
+
+const liNumSix = document.querySelectorAll('.book-6-li'); // получили все li
+// console.log(liNumSix);
+liNumSix[8].after(liNumSix[10]);
