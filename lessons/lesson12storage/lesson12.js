e82273d19d099ad8e13f13ac70706a32aac31564
Alextxnk <alexsolov766@gmail.com>
2022-12-29 19:09:19 +0300

    lesson 12

--- lessons/lesson12storage/lesson12.js
+++ lessons/lesson12storage/lesson12.js
@@ -42,8 +42,36 @@ showText();
 // cookie содержатся в объекте document
 
 // пример
-document.cookie = 'имя=значение';
+/* document.cookie = 'имя=значение';
 document.cookie = 'имя2=значение2';
 document.cookie = 'имя3=значение3';
 document.cookie = 'имя=значение4'; // в таком случае заменятеся значение в первой
 
+
+// по-умолчанию куки хранятся сессию 
+*/
+// время, до которого будут храниться куки
+document.cookie = 'hope=life; expires=Tue, 7 May 2024 00:00:00 GMT';  
+
+
+function setCookie(key, value, year, month, day, path, domain, secure) {
+   let cookieStr = encodeURI(key) + '=' + encodeURI(value); 
+   if (year) {
+      const expires = new Date(year, month - 1, day);
+      cookieStr += '; expires=' + expires.toGMTString(); 
+   }
+
+   cookieStr += path ? '; path=' + encodeURI(path) : '';
+   cookieStr += domain ? '; domain=' + encodeURI(domain) : '';
+   cookieStr += secure ? '; secure' : '';
+
+   document.cookie = cookieStr;
+}
+
+setCookie('Hello', 'world');
+setCookie('New', 'Cookie', 2023, 5, 3);
+
+// console.log(document.cookie); // выводит все куки: имя2=значение2; имя3=значение3; имя=значение4
+console.log(decodeURI(document.cookie));
+
+// path, domain, key, value лучше кодировать в URI
---
+++ lessons/lesson12storage/toDoList/scripts/script.js
@@ -0,0 +1,72 @@
+'use strict';
+
+
+const todoControl = document.querySelector('.todo-control'); // форма
+const headerInput = document.querySelector('.header-input'); // инпут формы
+const todoList = document.querySelector('.todo-list'); // новые дела
+const todoCompleted = document.querySelector('.todo-completed'); // выполненые дела
+
+// массив дел
+const todoData = [
+   /* {
+      value: 'Сварить кофе',
+      completed: false
+   },
+   {
+      value: 'Помыть посуду',
+      completed: true
+   } */
+];
+
+const render = function() {
+   todoList.textContent = '';
+   todoCompleted.textContent = '';
+
+   todoData.forEach(function(item) {
+      const li = document.createElement('li');
+      li.classList.add('todo-item');
+
+      li.innerHTML = `
+      <span class="text-todo">${item.value}</span>
+      <div class="todo-buttons">
+         <button class="todo-remove"></button>
+         <button class="todo-complete"></button>
+      </div>
+      `;
+
+      if(item.completed) {
+         todoCompleted.append(li);
+      } else {
+         todoList.append(li);
+      }
+
+      const toDoCompleteBtn = li.querySelector('.todo-complete');
+      toDoCompleteBtn.addEventListener('click', function() {
+         item.completed = !item.completed;
+         render(); // рекурсивно вызываем функцию
+      });
+
+   });
+};
+
+todoControl.addEventListener('submit', function(event) {
+   event.preventDefault(); // теперь у нас страница не перезагружается 
+
+   const newTodo = {
+      value: headerInput.value,
+      completed: false
+   };
+
+   todoData.push(newTodo);
+
+   render(); // после вызываем рендер, чтоб у нас обновилась страница 
+});
+
+render(); // вызываем ее, как только запустилась страница 
+
+
+// что нужно реализовать:
+// удаление записи
+// после добавления дела, поле "Какие планы?" должно очищаться 
+// нельзя добавлять пустое поле 
+// сохранять все необходимо в Local Storage и записывать из нее в массив todoData
